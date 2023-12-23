c0c6ee2c8295564859730facfa057856f321df54
priyanshusibu <154685381+priyanshusibu@users.noreply.github.com>
2023-12-23 20:09:52 +0530

    Add files via upload

---
+++ Pattren24.cpp-.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=7;i++)
+    {
+      
+    	for(int j=1;j<=7;j++)
+    	{	
+    		if(i==1 || i==7 || j==1 || j==7)
+    		{
+    		  printf("*");
+    	    }
+			else
+			{
+			 printf(" ");
+			}
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
